7d4c2d940c64dba09aec8d4d89aa937694626951
Seung Ju <kingsky32@gmail.com>
2021-11-14 15:21:56 +0900

    Setup OpenAPI With Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppConfigService } from '#config/app/config.service';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import '#common/utils/env';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Seung Ju Server')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   // Get app config for cors settings and starting the app.
   const appConfig = app.get<AppConfigService>(AppConfigService);
   await app.listen(appConfig.port);
