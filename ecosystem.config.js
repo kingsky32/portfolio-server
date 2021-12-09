487b6d47d41158f1ee420a251229dc03c3e45a7e
Seung Ju <kingsky32@gmail.com>
2021-12-10 00:41:17 +0900

    Update PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,13 @@ module.exports = {
     {
       name: 'seung-ju-server',
       script: './dist/main.js',
+      instances: 0,
+      autorestart: true,
       watch: false,
       exec_mode: 'cluster',
+
+      output: '~/logs/pm2/console.log',
+      error: '~/logs/pm2/onsoleError.log',
     },
   ],
 };
